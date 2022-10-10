9199d54514a152982884153a73a4dcd104049a54
hsmkp4 <hesam.lfl@gmail.com>
2022-10-10 20:21:40 +0330

    Add logic for fetch weather data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { fetchWeatherByCity } from "../utils/fetchAPI";
 import Search from "../components/Search";
 
-export default function Home() {
+export default function Home({ weather }) {
+  const [city, setCity] = useState("tehran");
+  const router = useRouter();
+
+  console.log(weather);
+  // make query params for each city
+  useEffect(() => {
+    router.push(`/?city=${city}`);
+  }, [city]);
+
+  const handleSearch = (newCity) => {
+    setCity(newCity);
+  };
   return (
     <div>
       <h1>Weather App</h1>
-      <Search />
+      <Search handleSearch={handleSearch} />
     </div>
   );
+}
+
+export async function getServerSideProps({ query }) {
+  // fetch weather info in server side
+  const weather = await fetchWeatherByCity(query["city"]);
+
+  return {
+    props: {
+      weather,
+    },
+  };
 }
---
+++ utils/fetchAPI.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const URL = "http://api.openweathermap.org/data/2.5";
+const KEY = "19a67b87919bdded758ac2738b8372f2";
+
+export const fetchWeatherByCity = async (city = "tehran") => {
+  const { data } = await axios(`${URL}/weather?q=${city}&APPID=${KEY}`);
+  return data;
+};
